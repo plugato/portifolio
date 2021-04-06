06450c2af429e18bd27d397d458abd8c651c990d
ancalagon <manoel.azevedo@gazin.com.br>
2021-04-06 19:37:45 -0300

    Initial commit

---
+++ .next/static/C5KNB7stK0tvhKoSvUagh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-90b1c92dec0dda1d856c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-be31f7ab81316eb87c58.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
